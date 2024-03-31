87a308fb5e025bad6dd33afd8357b2064238aaf4
Ryan-Erickson911 <ryanerickson407@gmail.com>
2024-03-31 16:36:56 -0600

    line update

--- lib/data/users.dart
+++ lib/data/users.dart
@@ -1,7 +1,7 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
 
-
+final supabase = Supabase.instance.client;
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
 
